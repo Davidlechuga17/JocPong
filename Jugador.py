05f87c1e8608182ba03a1ed7b586e4f4dcebbe26
David <davidlechuga@ceroca.cat>
2024-04-17 15:50:01 +0200

    mostrant jugadors

---
+++ Jugador.py
@@ -0,0 +1,7 @@
+class jugador():
+
+    def __init__(self, posX, posY, color, vel):
+        self.posX = posX
+        self.posY = posY
+        self.color = color
+        self.vel = vel
---
+++ constants.py
@@ -0,0 +1,33 @@
+
+class Constants():
+
+    AMPLE_ESCENARI = 600
+
+    ALCADA_ESCENARI = 400
+
+    VERD = (0,255, 0)
+
+    BLAU = (0, 0, 255)
+
+    POSICIO_MARGE_DE_DALT = (0, 0, 600, 10)
+
+    POSICIO_MARGE_DE_BAIX = (0, 390, 600, 10)
+
+    POSICIOJ1 = (10,180, 10, 40)
+    POSICIOJ2 = (580,180, 10,40)
+
+    COLOR_J1 = (255, 0, 0)
+    COLOR_J2= (85,0,255)
+
+
+
+#MIDA_FINESTRA = pygame.display.set_mode((600, 400))
+
+#COLOR_FONS_VERD = finestraJoc.fill((0,155, 0))
+
+#MARGE_DE_DALT = pygame.draw.rect(finestraJoc, (0, 0, 255), (0, 0, 600, 10))
+
+#MARGE_DE_BAIX =  pygame.draw.rect(finestraJoc, (0, 0, 255), (0, 390, 600, 10))
+
+
+
