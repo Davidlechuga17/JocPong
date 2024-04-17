8c7d789078439f415f971180065c8e8400d8439b
David <davidlechuga@ceroca.cat>
2024-04-17 16:36:25 +0200

    Moviment dels jugadors fet (segon pdf)

--- constants.py
+++ constants.py
@@ -13,11 +13,25 @@ class Constants():
 
     POSICIO_MARGE_DE_BAIX = (0, 390, 600, 10)
 
-    POSICIOJ1 = (10,180, 10, 40)
-    POSICIOJ2 = (580,180, 10,40)
+    #POSICIOJ1 = (10,180, 10, 40)
+    #POSICIOJ2 = (580,180, 10,40)
 
     COLOR_J1 = (255, 0, 0)
     COLOR_J2= (85,0,255)
+
+    def MovimentJugador1(teclas, pygame, jugador1):
+        if teclas[pygame.K_w] and jugador1.posY>Constants.POSICIO_MARGE_DE_DALT[3]:
+            jugador1.posY -= jugador1.vel
+
+        if teclas[pygame.K_s] and jugador1.posY<Constants.ALCADA_ESCENARI-Constants.POSICIO_MARGE_DE_BAIX[3]-41:
+            jugador1.posY += jugador1.vel
+
+    def MovimentJugador2(teclas, pygame, jugador2):
+        if teclas[pygame.K_UP] and jugador2.posY>Constants.POSICIO_MARGE_DE_DALT[3]:
+            jugador2.posY -= jugador2.vel
+
+        if teclas[pygame.K_DOWN] and jugador2.posY<Constants.ALCADA_ESCENARI-Constants.POSICIO_MARGE_DE_BAIX[3]-41:
+            jugador2.posY += jugador2.vel
 
 
 
--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import pygame
 
 from constants import Constants
 
-
-
 pygame.init()
 
 finestraJoc = pygame.display.set_mode((Constants.AMPLE_ESCENARI, Constants.ALCADA_ESCENARI))
@@ -18,42 +16,30 @@ def pintaObjectes():
     finestraJoc.fill(Constants.VERD)
     pygame.draw.rect(finestraJoc, Constants.BLAU, Constants.POSICIO_MARGE_DE_DALT)
     pygame.draw.rect(finestraJoc, Constants.BLAU, Constants.POSICIO_MARGE_DE_BAIX)
-    pygame.draw.rect(finestraJoc, Constants.COLOR_J1, Constants.POSICIOJ1)
-    pygame.draw.rect(finestraJoc, Constants.COLOR_J2, Constants.POSICIOJ2)
+    pygame.draw.rect(finestraJoc, Constants.COLOR_J1, (jugador1.posX, jugador1.posY, 10, 40))
+    pygame.draw.rect(finestraJoc, Constants.COLOR_J2, (jugador2.posX, jugador2.posY, 10, 40))
 
-def DetectaEvents():
+def DetectaEvents(jugador1, jugador2):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  # event per tancar la finestra amb el botó de tancar finestres del sistema.
             sys.exit()  # fa falta afegir un import sys
 
     teclas =  pygame.key.get_pressed()
 
-    if teclas[pygame.K_w]:
-        jugador1.posY + jugador1.vel/2
+    Constants.MovimentJugador1(teclas, pygame, jugador1)
 
-    if teclas[pygame.K_s]:
-        jugador1.posY - jugador1.vel/2
-
-    if teclas[pygame.K_UP]:
-        jugador2.posY + jugador2.vel/2
-
-    if teclas[pygame.K_DOWN]:
-        jugador2.posY - jugador2.vel/2
-
+    Constants.MovimentJugador2(teclas, pygame, jugador2)
 
 from Jugador import jugador
 
-jugador1 = jugador(posX=5, posY=20, color=Constants.COLOR_J1, vel=5)
-jugador2 = jugador(posX=10, posY=40, color=Constants.COLOR_J2, vel=5)
+jugador1 = jugador(posX=10, posY=180, color=Constants.COLOR_J1, vel=5)
+jugador2 = jugador(posX=580, posY=180, color=Constants.COLOR_J2, vel=5)
 
 while not gameOver:
 
     pintaObjectes()
 
-    DetectaEvents()
+    DetectaEvents(jugador1, jugador2)
 
     rellotge.tick(30)
     pygame.display.update()
-
-
-
